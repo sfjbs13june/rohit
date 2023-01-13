4e46efe652d8b0778c2f43e568fd47d00d1312ae
rohit <r.swarnkar1999@gmail.com>
2023-01-13 05:23:05 +0000

    Day02

---
+++ Day02/first-springboot-project/src/main/java/com/rohit/app/FirstSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringbootProjectApplication.class, args);
+	}
+
+}
