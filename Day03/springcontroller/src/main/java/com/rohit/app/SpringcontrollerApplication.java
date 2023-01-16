f9fbeffa48a3c230bfb110b63d9b3b809df1e926
rohit <r.swarnkar1999@gmail.com>
2023-01-16 04:54:56 +0000

    Day 03

---
+++ Day03/springcontroller/src/main/java/com/rohit/app/SpringcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.rohit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcontrollerApplication.class, args);
+	}
+
+}
