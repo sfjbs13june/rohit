5e9303b055871fc6866fbaadf064591b9752d0a5
rohit <r.swarnkar1999@gmail.com>
2023-02-02 07:09:57 +0000

    Assignment16

---
+++ Day16/spring-artemis-message-app/artemis-message-consumer/src/main/java/com/rohit/app/ArtemisConsumerApp.java
@@ -0,0 +1,11 @@
+package com.rohit.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisConsumerApp {
+	public static void main(String[] args) {
+		SpringApplication.run(ArtemisConsumerApp.class, args);
+	}
+}
