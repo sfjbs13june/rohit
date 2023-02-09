53581710d8bee687a432fff469c48ee6927e8d61
rohit <r.swarnkar1999@gmail.com>
2023-02-09 07:11:26 +0000

    update-hospital-management-app

--- Day 18/hospital-management-app/src/test/java/com/rohit/app/DoctorControllerTest.java
+++ Day 18/hospital-management-app/src/test/java/com/rohit/app/DoctorControllerTest.java
@@ -1,6 +1,32 @@
 package com.rohit.app;
 
+import com.rohit.app.controller.DoctorController;
+import com.rohit.app.repository.AppointmentRepository;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class DoctorControllerTest
 {
+    @InjectMocks
+    private DoctorController doctorController;
 
+    @Mock
+    private AppointmentRepository appointmentRepository;
+
+    @BeforeEach
+    void setUp()
+    {
+        appointmentRepository = Mockito.mock(AppointmentRepository.class);
+    }
+
+    @Test
+    public void getDoctorAppointmentTest()
+    {
+    }
 }
